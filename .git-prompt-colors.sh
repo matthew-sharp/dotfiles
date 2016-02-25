212ee20ba5a4e143cec06b7343bc26c65a2b0557
Matthew Sharp <matthew.sharp@quantium.com.au>
2016-02-26 10:56:15 +1100

    Change git prompt to green

--- .git-prompt-colors.sh
+++ .git-prompt-colors.sh
@@ -2,7 +2,7 @@
 override_git_prompt_colors() {
   GIT_PROMPT_THEME_NAME="Single_line"
 
-  GIT_PROMPT_START_USER="_LAST_COMMAND_INDICATOR_ ${BoldYellow}${PathShort}${ResetColor}"
+  GIT_PROMPT_START_USER="_LAST_COMMAND_INDICATOR_ ${BoldGreen}${PathShort}${ResetColor}"
   GIT_PROMPT_END_USER="${ResetColor} $ "
   GIT_PROMPT_END_ROOT="${BoldRed} # "
 }
